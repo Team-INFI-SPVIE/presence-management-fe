31508f17d017b83bb8b66bd04a7d35304cdc65d4
benthebagnan <benthebagnan@gmail.com>
2022-11-09 17:27:53 +0000

    Resolve conflict project

--- src/app/features/admin/presence-management/presence-management/presence-management.component.ts
+++ src/app/features/admin/presence-management/presence-management/presence-management.component.ts
@@ -3,8 +3,8 @@ import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Presence, StudentsPresenses } from 'src/models/users.model';
-import { PresenceModalComponent } from '../components/presence-modal/presence-modal/presence-modal.component';
-import { Presence } from 'src/models/users.model';
+// import { PresenceModalComponent } from '../components/presence-modal/presence-modal/presence-modal.component';
+// import { Presence } from 'src/models/users.model';
 
 
 
@@ -17,8 +17,9 @@ export class PresenceManagementComponent implements OnInit {
 
   
   user!: any
-  modalRef: MdbModalRef<PresenceModalComponent> | null = null;
+  // modalRef: MdbModalRef<PresenceModalComponent> | null = null;
   presences!: Presence[]
+  lastPresences$!:Observable<Presence>
   
 
   studentsPresenses!: StudentsPresenses[] | any
@@ -40,41 +41,44 @@ export class PresenceManagementComponent implements OnInit {
 
   picture = "https://bigdataci.net/storage/photos/0UKpM7a15oI11obrDqyqtKwDMh9PLdLSgmCb4Hw6.png"
 
+  // constructor(
+  //   // private modalService: MdbModalService,
+  //   private apiService: ApiService,
+  //   private authService: AuthService
+  // ) { }
+
+  // ngOnInit(): void {
+  //   this.user = this.authService.getCurrentUser()
+  //   // this.presences = this.apiService.getPresences()
+  //   this.studentsPresenses = this.apiService.getstudentsPresenses()
+  //   // console.log(this.apiService.getstudentsPresenses());
+    
+  // }
+
+  // openModal(professor: any) {
+  //   this.modalRef = this.modalService.open(PresenceModalComponent, {
+  //     data: professor,
+  //   });
+  // }
+
+  // getValueFromSelect(selectedCategory: any) {
+  //   console.log(selectedCategory);
+    
+
+  // lastPresences$!: Observable<Presence>
+
   constructor(
-    private modalService: MdbModalService,
     private apiService: ApiService,
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    this.user = this.authService.getCurrentUser()
-    this.presences = this.apiService.getPresences()
-    this.studentsPresenses = this.apiService.getstudentsPresenses()
-    console.log(this.apiService.getstudentsPresenses());
-    
-  }
-
-  openModal(professor: any) {
-    this.modalRef = this.modalService.open(PresenceModalComponent, {
-      data: professor,
-    });
-  }
-
-  getValueFromSelect(selectedCategory: any) {
-    console.log(selectedCategory);
-    
-
-  lastPresences$!: Observable<Presence>
-
-  constructor(
-    private apiService: ApiService
-  ) { }
-
-  ngOnInit(): void {
     this.lastPresences$ = this.apiService.getLatestPresense();
-
+    this.user = this.authService.getCurrentUser()
   }
 
   
 
 }
+
+
